9e27df6d2e3ef912b6b4dde4d13d09e7c6338677
MaksSvid <sviderskiymaks04@gmail.com>
2026-05-03 14:18:43 +0100

    updates

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# NOTE: use "chmod +x init.sh" to make this file executable
+
+set -e # Exit immediately if a command exits with a non zero status
+set -u # Treat unset variables as an error when substituting
